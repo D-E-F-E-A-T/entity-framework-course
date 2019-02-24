a299fdc5180cb509afe56bedbe6eaa00f3e15ca6
danielccunha <danielcunha54@gmail.com>
2019-02-24 16:17:38 -0300

    Initialized project

---
+++ DatabaseFirstDemo/DatabaseFirstDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DatabaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DatabaseFirstDemoEntities();
+            var @event = new Events
+            {
+                Title = "Second title",
+                Description = "Second description",
+                LastChange = DateTime.Now
+            };
+
+            context.Events.Add(@event);
+            context.SaveChanges();
+        }
+    }
+}
