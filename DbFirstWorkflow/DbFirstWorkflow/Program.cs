8e9607e85671cc57a941076f6fd7103ebfc2630c
danielccunha <danielcunha54@gmail.com>
2019-02-26 13:06:22 -0300

    Finished database first workflow section

--- DbFirstWorkflow/DbFirstWorkflow/Program.cs
+++ DbFirstWorkflow/DbFirstWorkflow/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace DbFirstWorkflow
+﻿using System;
+
+namespace DbFirstWorkflow
 {
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-
+            Course course = new Course
+            {
+                Level = Level.Advanced
+            };
         }
     }
 }
