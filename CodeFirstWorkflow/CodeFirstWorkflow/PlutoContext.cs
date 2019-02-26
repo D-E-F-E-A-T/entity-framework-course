e3c94eced74251ad34b87496324b7dad3ff1fff3
danielccunha <danielcunha54@gmail.com>
2019-02-26 13:29:48 -0300

    Initialized CodeFirstWorkflow project

---
+++ CodeFirstWorkflow/CodeFirstWorkflow/PlutoContext.cs
@@ -0,0 +1,17 @@
+﻿using CodeFirstWorkflow.Models;
+using System.Data.Entity;
+
+namespace CodeFirstWorkflow
+{
+    public class PlutoContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public PlutoContext() : base("name=DefaultConnection")
+        {
+
+        }
+    }
+}
