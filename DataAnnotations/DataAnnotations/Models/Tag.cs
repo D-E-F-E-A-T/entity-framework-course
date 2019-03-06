e3135840b3fc670874d06005ae08bb9cf3d9c069
danielccunha <danielcunha54@gmail.com>
2019-03-05 22:44:24 -0300

    Created DataAnnotations project

---
+++ DataAnnotations/DataAnnotations/Models/Tag.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DataAnnotations.Models
+{
+    public class Tag
+    {
+        public Tag()
+        {
+            Courses = new HashSet<Course>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
