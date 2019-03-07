296d8cbbe2fc545cdb82a9eab693bf8635650dbb
danielccunha <danielcunha54@gmail.com>
2019-03-07 13:06:41 -0300

    Initialized LINQ section

---
+++ Queries/Queries/PlutoContext.cs
@@ -0,0 +1,23 @@
+﻿using Queries.EntityConfigurations;
+using Queries.Models;
+using System.Data.Entity;
+
+namespace Queries
+{
+    public class PlutoContext : DbContext
+    {
+        public PlutoContext() : base("name=PlutoContext")
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+        public virtual DbSet<Cover> Covers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CourseConfiguration());
+        }
+    }
+}
---
+++ Queries/Queries/Program.cs
@@ -0,0 +1,31 @@
+﻿using Queries.Models;
+using System;
+using System.Linq;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlutoContext context = new PlutoContext();
+
+            // LINQ syntax
+            var query = from course in context.Courses
+                        where course.Name.ToLower().Contains("c#")
+                        orderby course.Name
+                        select course;
+
+            foreach (Course course in query)
+                Console.WriteLine(course.Name);
+
+            // Extension methods
+            var courses = context.Courses
+                .Where(course => course.Name.ToLower().Contains("c#"))
+                .OrderBy(course => course.Name);
+
+            foreach (Course course in courses)
+                Console.WriteLine(course.Name);
+        }
+    }
+}
